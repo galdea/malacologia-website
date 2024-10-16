6b66ba90ff8a05d4545989a7728654aad9cf269c
Gabriel Aldea <109429160+galdea@users.noreply.github.com>
2024-10-16 21:26:26 +0000

    vercel

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,4 +1,3 @@
-
 'use client'
 import AOS from 'aos'
 import { useEffect, useState } from "react"
@@ -10,36 +9,38 @@ import Header25 from './header/Header25'
 
 export default function Layout({ headerStyle, footerStyle, breadcrumbTitle, children }) {
     const [scroll, setScroll] = useState(0)
-    // Moblile Menu
+    // Mobile Menu
     const [isMobileMenu, setMobileMenu] = useState(false)
     const handleMobileMenu = () => setMobileMenu(!isMobileMenu)
 
     useEffect(() => {
-        // const WOW = require('wowjs')
-        // window.wow = new WOW.WOW({
-        //     live: false
-        // })
-        // window.wow.init()
         AOS.init()
 
-        document.addEventListener("scroll", () => {
+        const handleScroll = () => {
             const scrollCheck = window.scrollY > 100
             if (scrollCheck !== scroll) {
                 setScroll(scrollCheck)
             }
-        })
-    }, [])
+        }
+
+        document.addEventListener("scroll", handleScroll)
+
+        // Cleanup event listener on component unmount
+        return () => {
+            document.removeEventListener("scroll", handleScroll)
+        }
+    }, [scroll]) // Add scroll as a dependency
+
     return (
         <>
-            {headerStyle == 25 ? <Header25 scroll={scroll} isMobileMenu={isMobileMenu} handleMobileMenu={handleMobileMenu} /> : null}
-            <MobileMenu  isMobileMenu={isMobileMenu} handleMobileMenu={handleMobileMenu}/>
-
+            {headerStyle === 25 ? <Header25 scroll={scroll} isMobileMenu={isMobileMenu} handleMobileMenu={handleMobileMenu} /> : null}
+            <MobileMenu isMobileMenu={isMobileMenu} handleMobileMenu={handleMobileMenu} />
 
             {breadcrumbTitle && <Breadcrumb breadcrumbTitle={breadcrumbTitle} />}
 
             {children}
 
-            {footerStyle == 13 ? <Footer13 /> : null} {/* Render only Footer13 */}
+            {footerStyle === 13 ? <Footer13 /> : null} {/* Render only Footer13 */}
 
             <BackToTop />
         </>
