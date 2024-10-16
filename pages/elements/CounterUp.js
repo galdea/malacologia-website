2c34a279ddbb223d69c22b63b15b611faffde3f2
Gabriel Aldea <109429160+galdea@users.noreply.github.com>
2024-10-16 21:19:54 +0000

    vercel

--- pages/elements/CounterUp.js
+++ pages/elements/CounterUp.js
@@ -1,31 +1,30 @@
 'use client'
-import { useEffect, useState } from 'react'
+import { useState, useRef } from 'react'
 import Counter from './Counter'
 
 export default function CounterUp({ count }) {
     const [inViewport, setInViewport] = useState(false)
+    const counterRef = useRef(null)
 
     const handleScroll = () => {
-        const elements = document.getElementsByClassName('counter')
-        if (elements.length > 0) {
-            const element = elements[0]
-            const rect = element.getBoundingClientRect()
+        if (counterRef.current) {
+            const rect = counterRef.current.getBoundingClientRect()
             const isInViewport = rect.top >= 0 && rect.bottom <= window.innerHeight
             if (isInViewport && !inViewport) {
                 setInViewport(true)
+                // Remove the scroll event listener once in viewport
+                window.removeEventListener('scroll', handleScroll)
             }
         }
     }
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll)
-        return () => {
-            window.removeEventListener('scroll', handleScroll)
-        }
-    }, [])
+    // Attach scroll event listener on component mount
+    window.addEventListener('scroll', handleScroll)
+
+    // Return the JSX
     return (
-        <>
-            <span className="counter">{inViewport && <Counter end={count} duration={20} />}</span>
-        </>
+        <span className="counter" ref={counterRef}>
+            {inViewport && <Counter end={count} duration={20} />}
+        </span>
     )
 }
