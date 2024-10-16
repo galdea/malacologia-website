a8552ca3b64f6f274e4bda9ca223c3e5ffaf52e0
Gabriel Aldea <109429160+galdea@users.noreply.github.com>
2024-10-16 21:31:08 +0000

    vercel

--- pages/elements/Preloader.js
+++ pages/elements/Preloader.js
@@ -1,15 +1,23 @@
+import Image from 'next/image';
+
 export default function Preloader() {
     return (
         <>
             <div id="pre-load">
                 <div id="loader" className="loader">
                     <div className="loader-container">
-                        <div className="loader-icon"><img src="/assets/images/logo/logos11.png" alt=" " /></div>
+                        <div className="loader-icon">
+                            <Image 
+                                src="/assets/images/logo/logos11.png" 
+                                alt="Loading logo" 
+                                width={100} // Specify the desired width
+                                height={100} // Specify the desired height
+                                priority // Optional: Use priority loading for the preloader image
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
-
-
         </>
     )
 }
